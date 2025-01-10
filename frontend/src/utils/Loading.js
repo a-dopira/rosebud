7fe9987bdc0d72742682cdcff95d881bb0d34ebe
antond <doublescreen50@gmail.com>
2025-01-10 22:21:37 +0200

    gitignore changes

---
+++ frontend/src/utils/Loading.js
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-md">
+      <svg 
+        className="w-24 h-24 animate-pulse" 
+        viewBox="0 0 96 96" 
+        xmlns="http://www.w3.org/2000/svg"
+      ></svg>
+    </div>
+  );
+};
+
+export default Loader;
