0ab05ad8ffeec065b54c1efd7fe6d680fe98ea60
antond <doublescreen50@gmail.com>
2025-03-28 19:23:02 +0200

    docker changes

--- backrose/backrose/settings.py
+++ backrose/backrose/settings.py
@@ -145,8 +145,13 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "build" / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
