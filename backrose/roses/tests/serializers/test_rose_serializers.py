9536a605fa7b5fa6548879977d1dbc6b647fda4c
antond <doublescreen50@gmail.com>
2025-07-25 01:47:16 +0300

    serializers improvements

--- backrose/roses/tests/serializers/test_rose_serializers.py
+++ backrose/roses/tests/serializers/test_rose_serializers.py
@@ -1,115 +1,33 @@
 import pytest
-from roses.models import Rose, Breeder
+from roses.models import Rose
 from roses.serializers import RoseSerializer, RoseListSerializer, RoseCreateSerializer
 
 pytestmark = pytest.mark.django_db
 
 
+@pytest.mark.django_db
 class TestRoseSerializer:
 
-    def test_expected_fields(self, rose_with_relations):
+    def test_nested_relations(self, rose_with_relations):
         serializer = RoseSerializer(instance=rose_with_relations)
         data = serializer.data
 
-        expected_fields = set(RoseSerializer.Meta.fields)
+        assert "breeder" in data and isinstance(data["breeder"], dict)
+        assert "group" in data and isinstance(data["group"], dict)
 
-        for field in expected_fields:
-            assert field in data
+        nested_lists = [
+            "feedings",
+            "foliages",
+            "photos",
+            "sizes",
+            "videos",
+            "pesticides",
+            "fungicides",
+        ]
+        for field in nested_lists:
+            assert field in data and isinstance(data[field], list)
 
-    def test_related_fields_data(self, rose_with_relations):
-        serializer = RoseSerializer(instance=rose_with_relations)
-        data = serializer.data
-
-        assert data["title"] == "fancy rose"
-        assert data["title_eng"] == "fancy rose in english"
-        assert data["breeder"]["name"] == "fancy breeder"
-        assert data["group"]["name"] == "fancy group"
-
-        assert len(data["feedings"]) == 1
-        assert data["feedings"][0]["basal"] == "fancy basal fertilizer"
-        assert data["feedings"][0]["leaf"] == "fancy leaf fertilizer"
-
-        assert len(data["foliages"]) == 1
-        assert data["foliages"][0]["foliage"] == "some tasty beer"
-
-        assert len(data["photos"]) == 1
-        assert data["photos"][0]["descr"] == "fancy photo description"
-        assert data["photos"][0]["year"] == 2023
-
-        assert len(data["sizes"]) == 1
-        assert data["sizes"][0]["width"] == "25.50"
-        assert data["sizes"][0]["height"] == "30.20"
-
-        assert len(data["videos"]) == 1
-        assert data["videos"][0]["video"] == "https://example.com/test-video"
-        assert data["videos"][0]["descr"] == "fancy video description"
-
-        if len(data["rosepesticides"]) > 0:
-            assert (
-                data["rosepesticides"][0]["pesticide"]["name"]
-                == "fancy pesticide treatment"
-            )
-            assert len(data["rosepesticides"][0]["pesticide"]["pests"]) == 1
-            assert (
-                data["rosepesticides"][0]["pesticide"]["pests"][0]["name"]
-                == "fancy pest"
-            )
-
-        if len(data["rosefungicides"]) > 0:
-            assert (
-                data["rosefungicides"][0]["fungicide"]["name"]
-                == "fancy fungicide treatment"
-            )
-            assert len(data["rosefungicides"][0]["fungicide"]["fungi"]) == 1
-            assert (
-                data["rosefungicides"][0]["fungicide"]["fungi"][0]["name"]
-                == "fancy fungus"
-            )
-
-    def test_create_rose(self, breeder, group, fancy_image):
-        data = {
-            "title": "New fancy rose",
-            "title_eng": "New fancy rose in english",
-            "group": group.id,
-            "breeder": breeder.id,
-            "photo": fancy_image,
-            "description": "New Description",
-            "landing_date": "2023-05-20",
-            "observation": "New Observation",
-            "susceptibility": "Medium",
-            "const_width": "12.5",
-            "const_height": "18.2",
-        }
-
-        serializer = RoseCreateSerializer(data=data)
-        assert serializer.is_valid(), f"Ошибки валидации: {serializer.errors}"
-
-        rose = serializer.save()
-        assert rose.title == "New fancy rose"
-        assert rose.title_eng == "New fancy rose in english"
-        assert rose.breeder == breeder
-        assert rose.group == group
-        assert rose.description == "New Description"
-
-    def test_update_rose(self, rose):
-        data = {
-            "title": "Updated Rose",
-            "title_eng": "Updated Rose English",
-            "group": rose.group.id,
-            "breeder": rose.breeder.id,
-            "description": "Updated Description",
-        }
-
-        serializer = RoseCreateSerializer(instance=rose, data=data, partial=True)
-        assert serializer.is_valid(), f"Ошибки валидации: {serializer.errors}"
-
-        updated_rose = serializer.save()
-        assert updated_rose.title == "Updated Rose"
-        assert updated_rose.title_eng == "Updated Rose English"
-        assert updated_rose.description == "Updated Description"
-        assert updated_rose.slug == "updated-rose"
-
-    def test_default_photo_value(self, breeder, group):
+    def test_default_photo_behavior(self, breeder, group):
         rose_without_photo = Rose.objects.create(
             title="rose without photo",
             title_eng="rose without photo in english",
@@ -118,80 +36,64 @@ class TestRoseSerializer:
         )
 
         serializer = RoseListSerializer(instance=rose_without_photo)
-        data = serializer.data
-
-        assert data["photo"] == "/media/images/cap_rose.png"
-
-
-class TestRoseListSerializer:
-
-    def test_serializer_list_of_roses(self, rose, group):
-        breeder2 = Breeder.objects.create(name="second breeder")
-
-        rose2 = Rose.objects.create(
-            title="second rose",
-            title_eng="second rose in english",
-            photo="images/cap_rose.png",
-            group=group,
-            breeder=breeder2,
-        )
-
-        roses = Rose.objects.all()
-        serializer = RoseListSerializer(roses, many=True)
-        data = serializer.data
-
-        assert len(data) == 2
-        for item in data:
-            assert set(item.keys()) == {"id", "title", "photo", "group"}
-            assert isinstance(item["photo"], str)
-
-    def test_list_serializer_contains_expected_fields(self, rose):
-        serializer = RoseListSerializer(instance=rose)
-        data = serializer.data
-
-        expected_fields = ["id", "title", "photo", "group"]
-
-        for field in expected_fields:
-            assert field in data
-
-        excluded_fields = ["title_eng", "breeder", "description", "landing_date"]
-
-        for field in excluded_fields:
-            assert field not in data
+        assert serializer.data["photo"] == "/media/images/cap_rose.png"
 
 
 @pytest.mark.django_db
 class TestRoseCreateSerializer:
 
-    def test_create_serializer_fields(self):
-        serializer = RoseCreateSerializer()
-        expected_fields = {
-            "id",
-            "title",
-            "title_eng",
-            "group",
-            "breeder",
-            "photo",
-            "description",
-            "landing_date",
-            "observation",
-            "susceptibility",
-            "const_width",
-            "const_height",
-        }
-        assert set(serializer.fields.keys()) == expected_fields
-
     def test_create_rose(self, breeder, group, fancy_image):
+
         data = {
-            "title": "test rose",
-            "title_eng": "test rose eng",
+            "title": "fancy rose",
+            "title_eng": "fancy rose in english",
             "group": group.id,
             "breeder": breeder.id,
             "photo": fancy_image,
-            "description": "test description",
+            "description": "fancy rose description",
+            "const_width": "12.5",
+            "const_height": "18.2",
         }
-        serializer = RoseCreateSerializer(data=data)
-        assert serializer.is_valid(), f"Errors: {serializer.errors}"
 
-        instance = serializer.save()
-        assert instance.title == "test rose"
+        serializer = RoseCreateSerializer(data=data)
+        assert serializer.is_valid(), f"Validation errors: {serializer.errors}"
+
+        rose = serializer.save()
+        assert rose.title == "fancy rose"
+        assert rose.breeder == breeder
+        assert rose.group == group
+        assert rose.slug == "fancy-rose"
+
+
+@pytest.mark.django_db
+class TestRoseListSerializer:
+
+    def test_minimal_fields_only(self, rose):
+        serializer = RoseListSerializer(instance=rose)
+        data = serializer.data
+
+        assert set(data.keys()) == {"id", "title", "photo", "group"}
+
+        detailed_fields = ["feedings", "foliages", "pesticides", "description"]
+        for field in detailed_fields:
+            assert field not in data
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "serializer_class,action,expected_fields",
+    [
+        (RoseListSerializer, "list", {"id", "title", "photo", "group"}),
+        (
+            RoseCreateSerializer,
+            "create",
+            {"id", "title", "title_eng", "group", "breeder", "photo"},
+        ),
+    ],
+)
+def test_serializer_field_selection(rose, serializer_class, action, expected_fields):
+
+    serializer = serializer_class(instance=rose)
+    data_fields = set(serializer.data.keys())
+
+    assert expected_fields.issubset(data_fields)
--- backrose/roses/tests/serializers/test_serializers.py
+++ backrose/roses/tests/serializers/test_serializers.py
@@ -1,14 +1,13 @@
 import pytest
 from decimal import Decimal
-from datetime import date
 from django.db.models import Count
 from roses.serializers import (
     GroupSerializer,
     BreederSerializer,
     PesticideSerializer,
     FungicideSerializer,
-    FeedingSerializer,
-    FoliageSerializer,
+    PestSerializer,
+    FungusSerializer,
     SizeSerializer,
 )
 from roses.models import Rose, Group
@@ -16,26 +15,15 @@ from roses.models import Rose, Group
 
 @pytest.mark.django_db
 class TestGroupSerializer:
-
-    def test_serializer_check_expected_fields(self, group):
-        serializer = GroupSerializer(instance=group)
-        data = serializer.data
-
-        assert list(data.keys()) == ["id", "name", "rose_count"]
-        assert data["rose_count"] == 0
+    """testing rose count calculation"""
 
     def test_rose_count_calculation(self, group, breeder):
+
         Rose.objects.create(
-            title="fancy rose 1",
-            title_eng="fancy rose 1 in eng",
-            group=group,
-            breeder=breeder,
+            title="rose1", title_eng="rose1", group=group, breeder=breeder
         )
         Rose.objects.create(
-            title="fancy rose 2",
-            title_eng="fancy rose 2 in eng",
-            group=group,
-            breeder=breeder,
+            title="rose2", title_eng="rose2", group=group, breeder=breeder
         )
 
         group = Group.objects.annotate(rose_count=Count("roses")).get(id=group.id)
@@ -45,55 +33,12 @@ class TestGroupSerializer:
 
 
 @pytest.mark.django_db
-class TestBreederSerializer:
-
-    def test_serializer_check_expected_fields(self, breeder):
-        serializer = BreederSerializer(instance=breeder)
-        data = serializer.data
-
-        assert set(data.keys()) == {"id", "name"}
-        assert data["name"] == "fancy breeder"
-
-    def test_create_breeder(self):
-        data = {"name": "new breeder"}
-        serializer = BreederSerializer(data=data)
-        assert serializer.is_valid()
-
-        instance = serializer.save()
-        assert instance.name == "new breeder"
-
-    def test_update_breeder(self, breeder):
-        data = {"name": "updated breeder"}
-        serializer = BreederSerializer(instance=breeder, data=data)
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.name == "updated breeder"
-
-
-@pytest.mark.django_db
 class TestPesticideSerializer:
+    """many-to-many through pest_ids"""
 
-    def test_serializer_check_expected_fields(self, pesticide):
-        serializer = PesticideSerializer(instance=pesticide)
-        data = serializer.data
-
-        expected_fields = {"id", "name", "pests"}
-        assert set(data.keys()) == expected_fields
-
-    def test_serializer_pest_data(self, pesticide):
-        serializer = PesticideSerializer(instance=pesticide)
-        data = serializer.data
-
-        assert "pests" in data
-        assert isinstance(data["pests"], list)
-        assert len(data["pests"]) == 1
-        assert data["pests"][0]["name"] == "fancy pest"
-
-    def test_create_pesticide(self, rose, pest):
-
+    def test_create_with_pests(self, pest):
         data = {
-            "name": "new pesticide treatment",
+            "name": "new pesticide",
             "pest_ids": [pest.id],
         }
 
@@ -101,44 +46,24 @@ class TestPesticideSerializer:
         assert serializer.is_valid(), f"Errors: {serializer.errors}"
 
         instance = serializer.save()
-        assert instance.name == "new pesticide treatment"
         assert pest in instance.pests.all()
 
-    def test_update_pesticide(self, pesticide, pest):
-        data = {
-            "name": "updated pesticide name",
-            "pest_ids": [pest.id],
-        }
+    def test_nested_pest_data(self, pesticide):
+        serializer = PesticideSerializer(instance=pesticide)
+        data = serializer.data
 
-        serializer = PesticideSerializer(instance=pesticide, data=data)
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.name == "updated pesticide name"
-        assert pest in updated_instance.pests.all()
+        assert "pests" in data
+        assert len(data["pests"]) == 1
+        assert data["pests"][0]["name"] == "fancy pest"
 
 
 @pytest.mark.django_db
 class TestFungicideSerializer:
+    """many-to-many through fungi_ids"""
 
-    def test_serializer_contains_expected_fields(self, fungicide):
-        serializer = FungicideSerializer(instance=fungicide)
-        data = serializer.data
-        expected_fields = {"id", "name", "fungi"}
-        assert set(data.keys()) == expected_fields
-
-    def test_serializer_fungus_data(self, fungicide):
-        serializer = FungicideSerializer(instance=fungicide)
-        data = serializer.data
-
-        assert "fungi" in data
-        assert isinstance(data["fungi"], list)
-        assert len(data["fungi"]) == 1
-        assert data["fungi"][0]["name"] == "fancy fungus"
-
-    def test_create_fungicide(self, fungus):
+    def test_create_with_fungi(self, fungus):
         data = {
-            "name": "new fungicide treatment",
+            "name": "new fungicide",
             "fungi_ids": [fungus.id],
         }
 
@@ -146,191 +71,39 @@ class TestFungicideSerializer:
         assert serializer.is_valid(), f"Errors: {serializer.errors}"
 
         instance = serializer.save()
-        assert instance.name == "new fungicide treatment"
         assert fungus in instance.fungi.all()
-
-    def test_update_fungicide(self, fungicide, fungus):
-        data = {
-            "name": "updated fungicide name",
-            "fungi_ids": [fungus.id],
-        }
-
-        serializer = FungicideSerializer(instance=fungicide, data=data)
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.name == "updated fungicide name"
-        assert fungus in updated_instance.fungi.all()
-
-
-@pytest.mark.django_db
-class TestFeedingSerializer:
-
-    def test_serializer_contains_expected_fields(self, feeding):
-        serializer = FeedingSerializer(instance=feeding)
-        data = serializer.data
-
-        assert set(data.keys()) == set(
-            ["id", "basal", "basal_time", "leaf", "leaf_time"]
-        )
-
-    def test_serializer_values(self, feeding):
-        serializer = FeedingSerializer(instance=feeding)
-        data = serializer.data
-
-        assert data["basal"] == "fancy basal fertilizer"
-        assert data["basal_time"] == "2023-06-10"
-        assert data["leaf"] == "fancy leaf fertilizer"
-        assert data["leaf_time"] == "2023-06-20"
-
-    def test_create_feeding(self, rose):
-        data = {
-            "basal": "new basal fertilizer",
-            "basal_time": "2023-08-05",
-            "leaf": "new leaf fertilizer",
-            "leaf_time": "2023-08-15",
-        }
-
-        serializer = FeedingSerializer(data=data, context={"rose": rose})
-        assert serializer.is_valid()
-
-        instance = serializer.save(rose=rose)
-        assert instance.rose.id == rose.id
-        assert instance.basal == "new basal fertilizer"
-        assert instance.basal_time == date(2023, 8, 5)
-        assert instance.leaf == "new leaf fertilizer"
-        assert instance.leaf_time == date(2023, 8, 15)
-
-    def test_update_feeding(self, feeding):
-        data = {
-            "basal": "updated basal fertilizer",
-            "basal_time": "2023-09-05",
-            "leaf": "updated leaf fertilizer",
-            "leaf_time": "2023-09-15",
-        }
-
-        serializer = FeedingSerializer(
-            instance=feeding, data=data, context={"rose": feeding.rose}
-        )
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.basal == "updated basal fertilizer"
-        assert updated_instance.basal_time == date(2023, 9, 5)
-        assert updated_instance.leaf == "updated leaf fertilizer"
-        assert updated_instance.leaf_time == date(2023, 9, 15)
-
-
-@pytest.mark.django_db
-class TestFoliageSerializer:
-
-    def test_serializer_contains_expected_fields(self, foliage):
-        serializer = FoliageSerializer(instance=foliage)
-        data = serializer.data
-
-        assert set(data.keys()) == set(["id", "foliage", "foliage_time"])
-
-    def test_serializer_values(self, foliage):
-        serializer = FoliageSerializer(instance=foliage)
-        data = serializer.data
-
-        assert data["foliage"] == "some tasty beer"
-        assert data["foliage_time"] == "2023-07-05"
-
-    def test_create_foliage(self, rose):
-        data = {
-            "foliage": "new foliage description",
-            "foliage_time": "2023-08-25",
-        }
-
-        serializer = FoliageSerializer(data=data, context={"rose": rose})
-        assert serializer.is_valid()
-
-        instance = serializer.save(rose=rose)
-        assert instance.rose.id == rose.id
-        assert instance.foliage == "new foliage description"
-        assert instance.foliage_time == date(2023, 8, 25)
-
-    def test_update_foliage(self, foliage):
-        data = {
-            "foliage": "updated foliage description",
-            "foliage_time": "2023-09-25",
-        }
-
-        serializer = FoliageSerializer(
-            instance=foliage, data=data, context={"rose": foliage.rose}
-        )
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.foliage == "updated foliage description"
-        assert updated_instance.foliage_time == date(2023, 9, 25)
 
 
 @pytest.mark.django_db
 class TestSizeSerializer:
 
-    def test_serializer_contains_expected_fields(self, size):
-        serializer = SizeSerializer(instance=size)
-        data = serializer.data
-
-        assert set(data.keys()) == set(["id", "height", "width", "date_added"])
-
-    def test_serializer_values(self, size):
-        serializer = SizeSerializer(instance=size)
-        data = serializer.data
-
-        assert data["height"] == "30.20"
-        assert data["width"] == "25.50"
-        assert data["date_added"] == "2023-08-20"
-
-    def test_create_size(self, rose):
-        data = {
-            "height": "40.5",
-            "width": "35.7",
-            "date_added": "2023-08-30",
-        }
-
-        serializer = SizeSerializer(data=data, context={"rose": rose})
-        assert serializer.is_valid()
-
-        instance = serializer.save(rose=rose)
-        assert instance.rose.id == rose.id
-        assert instance.height == Decimal("40.5")
-        assert instance.width == Decimal("35.7")
-        assert instance.date_added == date(2023, 8, 30)
-
-    def test_update_size(self, size):
-        data = {
-            "height": "45.8",
-            "width": "38.2",
-            "date_added": "2023-09-30",
-        }
-
-        serializer = SizeSerializer(
-            instance=size, data=data, context={"rose": size.rose}
-        )
-        assert serializer.is_valid()
-
-        updated_instance = serializer.save()
-        assert updated_instance.height == Decimal("45.8")
-        assert updated_instance.width == Decimal("38.2")
-        assert updated_instance.date_added == date(2023, 9, 30)
-
     def test_read_only_id_field(self, size):
         data = {
-            "id": "poop",
+            "id": "should_be_ignored",
             "height": "50.0",
             "width": "40.0",
             "date_added": "2023-10-01",
         }
 
-        serializer = SizeSerializer(
-            instance=size, data=data, context={"rose": size.rose}
-        )
+        serializer = SizeSerializer(instance=size, data=data)
         assert serializer.is_valid()
 
         updated_instance = serializer.save()
-        assert updated_instance.id == size.id
+        assert updated_instance.id == size.id  # id doesn't change
         assert updated_instance.height == Decimal("50.0")
-        assert updated_instance.width == Decimal("40.0")
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "serializer_class,fixture_name,expected_fields",
+    [
+        (BreederSerializer, "breeder", {"id", "name"}),
+        (PestSerializer, "pest", {"id", "name"}),
+        (FungusSerializer, "fungus", {"id", "name"}),
+    ],
+)
+def test_simple_serializers(request, serializer_class, fixture_name, expected_fields):
+    instance = request.getfixturevalue(fixture_name)
+    serializer = serializer_class(instance=instance)
+
+    assert set(serializer.data.keys()) == expected_fields
